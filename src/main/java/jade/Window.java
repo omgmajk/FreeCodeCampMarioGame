c083406917f9d06ed40038d9e1ee940984123a0c
omgmajk <majkieboy@gmail.com>
2023-01-24 20:56:14 +0100

    Initial push.

---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,22 @@
+package jade;
+
+public class Window {
+
+    private int width, height;
+    private String title;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    public static Window get() {
+        if(Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+}
